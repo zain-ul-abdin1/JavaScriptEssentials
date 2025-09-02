53600188e5088d97741d3b12d3e92e3acd5c5268
unknown <zain.suleman1@icloud.com>
2025-09-02 23:58:48 +0500

    Data Types

---
+++ Js-2/basics-2.js
@@ -0,0 +1,45 @@
+
+// Undefined
+let a;
+console.log("a:",a,"| type:",typeof a); 
+
+//String
+let str1 = "Hello";
+let str2 = "World";
+
+console.log("str1:",str1,"| type:",typeof str1);
+console.log("str2:",str2,"| type:",typeof str2);
+
+//Numbers
+let num1 = 54;
+let num2 = 3.14;
+
+console.log("num1:",num1,"| type:",typeof num1);
+console.log("num2:",num2,"| type:",typeof num2);
+
+//Boolean
+let isTrue = true;
+let isFalse = false;
+
+console.log("isTrue:",isTrue,"| type:",typeof isTrue);
+console.log("isFalse:",isFalse,"| type:",typeof isFalse);
+
+//Null
+let emptyValue = null;
+console.log("emptyValue:",emptyValue,"| type:",typeof emptyValue);
+
+// Object
+let obj = { name: "John", age: 25 };
+console.log("obj:", obj, "| type:", typeof obj);
+
+// Array
+let arr = [1, 2, 3];
+console.log("arr:", arr, "| type:", typeof arr);
+
+// Function
+function greet() { return "Hi!"; }
+console.log("greet:", greet, "| type:", typeof greet);
+
+// Symbol
+let sym = Symbol("unique");
+console.log("sym:", sym, "| type:", typeof sym);
---
+++ Js-2/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>JS Practice</title>
+</head>
+<body>
+  <h1>Hello JavaScript</h1>
+  <script src="basics-2.js"></script>
+</body>
+</html>
