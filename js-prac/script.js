da7dba49d3be41899b12a3121016c99ec968c806
zain-ul-abdin1 <136232498+zain-ul-abdin1@users.noreply.github.com>
2026-06-25 17:13:45 +0500

    if clause added

--- js-prac/script.js
+++ js-prac/script.js
@@ -1,8 +1,15 @@
 const button = document.getElementById("calcBtn");
-button.onclick = function(){
-   const bill = document.getElementById('billAmount').value
+button.onclick = function () {
+    const billAmount = document.getElementById('billAmount').value;
+    if (billAmount === "") {
+        alert("Please enter Bill Amount");
+    }
+    else {
+        const bill = parseFloat(billAmount);    
+        const tip = bill * 0.15;
+        const total = bill + tip;
+        document.getElementById('displayResult').innerText = "Total bill Amount is : " + total + " Tip amount is : " + tip;
+    }
 
-   const tip = bill * 0.15;
 
-   document.getElementById('displayResult').innerText = "Tip amount is : "+ tip;
 }
